79d20e3559e17dccbede9235733c16854520e5f1
Vinicius Lellis de Andrade <viniciuslellis123@outlook.com>
2017-10-11 18:21:44 -0300

    Comentario de BD adicionado ao PROGRAM

--- Buffet/Program.cs
+++ Buffet/Program.cs
@@ -21,3 +21,67 @@ namespace Buffet
         }
     }
 }
+
+/*Banco de dados
+ 
+CREATE TABLE Cliente(
+  nome VARCHAR(64) NOT NULL,
+  endereco VARCHAR(64),
+  cpf BIGINT NOT NULL,
+  telefone BIGINT,
+  dataNasc DATE,
+  celular BIGINT,
+  numeroCasa INT,
+
+  PRIMARY KEY(cpf)
+);
+
+CREATE TABLE ClienteFisico(
+  nome VARCHAR(64) NOT NULL,
+  cpf BIGINT NOT NULL,
+  rg BIGINT,
+  telefone BIGINT,
+  celular BIGINT,
+  dataNasc DATE,
+  endereco VARCHAR(64),
+  bairro VARCHAR(64),
+  numeroCasa INT,
+
+  PRIMARY KEY(cpf)
+);
+/
+CREATE TABLE ClienteJuridico(
+  nomeEmpresa VARCHAR(64),
+  cnpj BIGINT NOT NULL,
+  cep BIGINT,
+  cidade VARCHAR(64),
+  rua VARCHAR(64),
+  bairro VARCHAR(64),
+  estado VARCHAR(64),
+  numeroEmpresa INTEGER,
+  representanteCPF BIGINT,
+
+  PRIMARY KEY(cnpj),
+  FOREIGN KEY (representanteCPF) REFERENCES RepresentanteJuridico
+);
+/
+CREATE TABLE RepresentanteJuridico(
+  nome VARCHAR(64) NOT NULL,
+  nacionalidade VARCHAR(64) DEFAULT 'Brasileiro',
+  estadoCivil VARCHAR(32),
+  profissao VARCHAR(64) DEFAULT 'Desempregado',
+  cpf BIGINT NOT NULL,
+  rg BIGINT,
+  telefone BIGINT,
+  cep BIGINT,
+  endereco VARCHAR(64),
+  bairro VARCHAR(64),
+  cidade VARCHAR(64),
+  estado VARCHAR(64),
+  numeroCasa INT,
+
+  PRIMARY KEY (cpf)
+);
+/
+
+     */
